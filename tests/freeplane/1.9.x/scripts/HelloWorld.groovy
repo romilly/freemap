662f95cd2a93e8f70dbd5c455e9bb4ede581f1e9
Romilly Cocking <romilly.cocking@gmail.com>
2021-12-22 11:14:55 +0000

    After deleting 1.9.x material

--- tests/freeplane/1.9.x/scripts/HelloWorld.groovy
+++
@@ -1,1 +0,0 @@
-node.text = 'Hello World'
--- tests/freeplane/1.9.x/scripts/hello.groovy
+++
@@ -1,8 +0,0 @@
-c.statusInfo = "Groovy's here:"
-mm = new File('/home/romilly/git/active/freemap/data/test1.mm').toURL()
-map = c.newMap(mm)
-Thread.sleep(1000)
-//c.export(map, new File('/home/romilly/git/active/freemap/data/test1.svg'), 'Scalable Vector Graphic (SVG) (.svg)',
-//true)
-c.export(map, new File('/home/romilly/git/active/freemap/data/test1.png'), 'Portable Network Graphic (PNG) (.png)',true)
-
