324072a898be29acc4adc7a75e973c7cfe195925
Romilly Cocking <romilly.cocking@gmail.com>
2022-01-20 10:24:18 +0000

    Added some sample files

---
+++ data/Connected-Mindmap-2.mm
@@ -0,0 +1,87 @@
+<map version="freeplane 1.7.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Connected&#xa;Mindmap" FOLDED="false" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1642343162527"><hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#808080ff,#808080ff,#808080ff,#808080ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600.0 px" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="5" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="one" POSITION="right" ID="ID_1594804264" CREATED="1642343165234" MODIFIED="1642343167150">
+<edge COLOR="#808080"/>
+<node TEXT="a" ID="ID_899538169" CREATED="1642343195801" MODIFIED="1642343270248">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1690628463" STARTINCLINATION="40;0;" ENDINCLINATION="40;0;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+</node>
+<node TEXT="b" ID="ID_1690628463" CREATED="1642343200010" MODIFIED="1642343202845"/>
+</node>
+<node TEXT="two" POSITION="right" ID="ID_539449377" CREATED="1642343167570" MODIFIED="1642343323568">
+<edge COLOR="#808080"/>
+<node TEXT="c" ID="ID_354888779" CREATED="1642343204634" MODIFIED="1642508518507">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_840211663" MIDDLE_LABEL="across" STARTINCLINATION="127;0;" ENDINCLINATION="203;0;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+</node>
+<node TEXT="d" ID="ID_1169123033" CREATED="1642343207866" MODIFIED="1642343323568"/>
+</node>
+<node TEXT="three" POSITION="left" ID="ID_1103489370" CREATED="1642343171546" MODIFIED="1642343173629">
+<edge COLOR="#808080"/>
+<node TEXT="e" ID="ID_783289891" CREATED="1642343223234" MODIFIED="1642508435382">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_682788039" STARTINCLINATION="40;0;" ENDINCLINATION="40;0;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+</node>
+<node TEXT="f" ID="ID_682788039" CREATED="1642343225633" MODIFIED="1642343226545"/>
+</node>
+<node TEXT="four" POSITION="left" ID="ID_1223243402" CREATED="1642343181938" MODIFIED="1642343280209">
+<edge COLOR="#808080"/>
+<node TEXT="g" ID="ID_1481660185" CREATED="1642343229001" MODIFIED="1642508647041">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1169123033" MIDDLE_LABEL="down" STARTINCLINATION="-3;123;" ENDINCLINATION="0;120;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+</node>
+<node TEXT="h" ID="ID_840211663" CREATED="1642343232938" MODIFIED="1642343280208"/>
+</node>
+</node>
+</map>
---
+++ notebooks/spikes/new-active-projects.mm
@@ -0,0 +1,260 @@
+<map version="freeplane 1.7.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node FOLDED="false" ID="ID_507487803" CREATED="1401002444040" MODIFIED="1641904369054" LINK="projects.mm" LOCALIZED_TEXT="absolute-Active&#10;&#10;Projects"><hook NAME="MapStyle">
+    <properties show_note_icons="true" fit_to_viewport="false;"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600.0 px" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="Rareblog" POSITION="right" ID="ID_826934628" CREATED="1381238207817" MODIFIED="1580804898409" LINK="../../../../git/active/RareBlog/plan/rareplan.mm">
+<node TEXT="marketing" ID="ID_656229546" CREATED="1555260932036" MODIFIED="1641724372059" LINK="../../../resources/marketing/Marketing.mm"/>
+<node TEXT="Build-a-brain" ID="ID_378529997" CREATED="1516895713229" MODIFIED="1615557979040" LINK="../../../../git/active/build-a-brain/plan/build-a-brain-plan.mm">
+<node TEXT="PyTorch" ID="ID_1875238187" CREATED="1554963015061" MODIFIED="1554963054054" LINK="../../../../git/active/pytorch/plan/pytorch-plan.mm"/>
+<node TEXT="Jetson nano" ID="ID_838129859" CREATED="1555664216095" MODIFIED="1555664309325" LINK="../../../../git/active/jetson-nano/plan/jetson-nano-plan.mm"/>
+<node TEXT="TensorFlow" ID="ID_317592980" CREATED="1555952045590" MODIFIED="1555952096421" LINK="../../../../git/active/tf-experiments/plan/tf-experiments-plan.mm"/>
+<node TEXT="baby learns to talk" ID="ID_288033574" CREATED="1599225756581" MODIFIED="1607519715644" LINK="../../../../git/active/baby-talk/plan/baby-talk-plan.mm"/>
+<node TEXT="c elegans" ID="ID_766166683" CREATED="1614617972414" MODIFIED="1614618003785" LINK="../../../../git/active/c-elegans/plan/c-elegans-plan.mm"/>
+</node>
+<node TEXT="bdd-tester" ID="ID_325599482" CREATED="1530866696450" MODIFIED="1530866700310">
+<node TEXT="lmm-talk-20180709" ID="ID_105364160" CREATED="1530866783864" MODIFIED="1530866783866" LINK="../../../../git/active/RareBlog/presentations/lmm-talk-20180709.mm">
+<edge STYLE="bezier" WIDTH="thin"/>
+</node>
+</node>
+<node TEXT="lazydoro" ID="ID_879179128" CREATED="1548504804367" MODIFIED="1549290377441" LINK="../../../../git/active/lazydoro/plan/lazydoro-plan.mm"/>
+<node TEXT="TiB" ID="ID_1030909582" CREATED="1555081396564" MODIFIED="1641832681901" LINK="../../../../git/active/tib/plan/TIB.mm"/>
+<node TEXT="InfoProducts" ID="ID_464540159" CREATED="1562671756403" MODIFIED="1605091179656" LINK="../../../../git/active/info-products/plan/info-products-plan.mm">
+<node TEXT="Deep learning on the Jetson Nano" ID="ID_1951276306" CREATED="1556028412766" MODIFIED="1639995279112" LINK="../../../../git/active/deep-nano/plan/deep-nano-plan.mm">
+<icon BUILTIN="button_cancel"/>
+</node>
+<node TEXT="start apl" ID="ID_1554333904" CREATED="1559024524375" MODIFIED="1639995283144" LINK="../../../../git/active/start-apl/plan/Start-APL.mm">
+<icon BUILTIN="button_cancel"/>
+</node>
+<node TEXT="AI Info products" ID="ID_732592867" CREATED="1562671774418" MODIFIED="1562671792806" LINK="../../../../git/active/ai-info-products/plan/AI-info-products-plan.mm"/>
+<node TEXT="sambot" ID="ID_513678747" CREATED="1564991256357" MODIFIED="1641832857398" LINK="../../../../git/active/sambot/plan/sambot-plan.mm"/>
+<node TEXT="LEPAT" ID="ID_1684802323" CREATED="1574864752706" MODIFIED="1574864862811" LINK="../../../../git/active/lepat/plan/lepat-plan.mm"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Learn Electronics in Practice and Theory
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="Nano and Chips cookbook" ID="ID_534859401" CREATED="1583138440548" MODIFIED="1639995422699" LINK="../../../../git/active/nano-and-chips/plan/nano-and-chips-plan.mm">
+<icon BUILTIN="idea"/>
+<icon BUILTIN="bookmark"/>
+<richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Guide to I2C, SPI, GPIO and Serial communications with the Nano.
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      Several parts:
+    </p>
+    <p>
+      Serial Communications
+    </p>
+    <p>
+      I2C and SPI
+    </p>
+    <p>
+      GPIO
+    </p>
+    <p>
+      Vision
+    </p>
+    <p>
+      Robotics
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="anastasia" ID="ID_168996509" CREATED="1583995167742" MODIFIED="1583995194805" LINK="../../../../git/active/anastasia-bot/plan/anastasia-bot-plan.mm">
+<node TEXT="private" ID="ID_497664339" CREATED="1584370088654" MODIFIED="1584370127178" LINK="../../../../git/active/anastasia-private/plan/anastasia-private-plan.mm"/>
+</node>
+<node TEXT="RUR" ID="ID_1598363556" CREATED="1583995199106" MODIFIED="1583995223291" LINK="../../../../git/active/RUR/plan/RUR-plan.mm"/>
+<node TEXT="multi VL53L0X plan" ID="ID_1471906995" CREATED="1585647038598" MODIFIED="1585647065035" LINK="/home/romilly/git/active/multi-VL53L0X/plan/multi-VL53L0X-plan.mm"/>
+<node TEXT="hat-tricks" ID="ID_491371270" CREATED="1575614165847" MODIFIED="1575614210688" LINK="../../../../git/active/explorer-hat/plan/hat-book.mm"/>
+<node TEXT="Making the Shrimp" ID="ID_1376266323" CREATED="1599907260505" MODIFIED="1641818972470" LINK="../../../../git/active/making-the-shrimp/plan/shrimplab-plan.mm">
+<icon BUILTIN="button_cancel"/>
+</node>
+<node TEXT="Kitronik Inventor's kit Python Companion" ID="ID_1568409525" CREATED="1601039074634" MODIFIED="1641833828621" LINK="../../../../git/active/kitronk-python-companion/plan/kitronk-python-companion-plan.mm"/>
+<node TEXT="APL remastered" ID="ID_411760608" CREATED="1611830809189" MODIFIED="1611830835568" LINK="../../../../git/active/apl-remastered/plan/apl-remastered-plan.mm"/>
+<node TEXT="scientific self development" ID="ID_1875975189" CREATED="1581245387106" MODIFIED="1641898138957" LINK="/home/romilly/git/active/scientific-self-development/plan/scientific-self-development-plan.mm"/>
+</node>
+<node TEXT="babelboard" ID="ID_403931033" CREATED="1562690952182" MODIFIED="1581777195219" LINK="../../../../git/active/babelboard-private/plan/babelboard-plan.mm"/>
+<node TEXT="tf-benchmarks" ID="ID_1250478130" CREATED="1563173279751" MODIFIED="1563173321123" LINK="../../../../git/active/tf-benchmarks/plan/tf-benchmarks-plan.mm"/>
+<node TEXT="breadboarder" ID="ID_1616145452" CREATED="1564157939895" MODIFIED="1564157970532" LINK="../../../../git/active/breadboarder/plan/Boarder-plan.mm"/>
+<node TEXT="arduino day 2020" ID="ID_754528068" CREATED="1579958552762" MODIFIED="1579958578806" LINK="../../../../git/active/arduino-day-2020/plan/arduino-day-2020-plan.mm"/>
+<node TEXT="fritzomatic" ID="ID_124506720" CREATED="1581545124575" MODIFIED="1581545152038" LINK="../../../../git/active/fritzomatic/plan/fritzomatic-plan.mm"/>
+<node TEXT="Dyalog" ID="ID_1786077563" CREATED="1558708772855" MODIFIED="1575366138585">
+<node TEXT="marketing for dyalog" ID="ID_1462561351" CREATED="1483616008989" MODIFIED="1484748373001" LINK="../../../../git/active/dyalog-marketing/plan/Marketing-for-Dyalog.mm"/>
+<node TEXT="XNORNet" ID="ID_199424416" CREATED="1558708781179" MODIFIED="1575366138582" LINK="../../../../git/active/xnor-net/plan/xnor-net-plan.mm"/>
+<node TEXT="APL ANN" ID="ID_287051542" CREATED="1581065208155" MODIFIED="1581065232253" LINK="../../../../git/active/apl-ann/plan/apl-ann-plan.mm"/>
+<node TEXT="CNN architectures" ID="ID_384885542" CREATED="1586354576478" MODIFIED="1586354605871" LINK="../../../../git/active/cnn-architectures/plan/cnn-architectures-plan.mm"/>
+<node TEXT="oxo" ID="ID_246220352" CREATED="1606900241511" MODIFIED="1607618158248" LINK="../../../../git/active/o-x-o/plan/o-x-o-plan.mm"/>
+<node TEXT="mamia" ID="ID_420369831" CREATED="1609580700643" MODIFIED="1609580765138" LINK="../../../../git/active/mamia/plan/mama-plan.mm"/>
+</node>
+<node TEXT="microplot" ID="ID_1105423777" CREATED="1614416882328" MODIFIED="1614416970129" LINK="../../../../git/active/microplot/plan/microplot-plan.mm"/>
+<node TEXT="Adafruit" ID="ID_876150905" CREATED="1614618699708" MODIFIED="1614618702941">
+<node TEXT="Clue" ID="ID_590326583" CREATED="1614618707164" MODIFIED="1614618724555" LINK="../../../../git/active/adafruit-clue/plan/adafruit-clue-plan.mm"/>
+<node TEXT="PyPortal" ID="ID_850144023" CREATED="1614618727116" MODIFIED="1614618745206" LINK="../../../../git/active/adafruit-pyportal/plan/adafruit-pyportal-plan.mm"/>
+</node>
+<node TEXT="synths" ID="ID_1080788387" CREATED="1615796358607" MODIFIED="1615796397033" LINK="../../../../git/active/synths/plan/synths-plan.mm"/>
+<node TEXT="Pi Pico" ID="ID_1087308754" CREATED="1616251947289" MODIFIED="1616252004219" LINK="../../../../git/active/pi-pico/plan/pi-pico-plan.mm">
+<node TEXT="Pico" ID="ID_67910232" CREATED="1616252021496" MODIFIED="1616252075220" LINK="../../../../git/active/picolinka/plan/picolinka-plan.mm"/>
+</node>
+<node TEXT="RARESchool" ID="ID_115044553" CREATED="1639215980573" MODIFIED="1639216025264" LINK="../../../../git/active/rareschool-private/plan/rareschool-private-plan.mm"/>
+</node>
+<node TEXT="Pip" POSITION="left" ID="ID_1688109334" CREATED="1401002524651" MODIFIED="1641818891207" LINK="../../../resources/pip/pip.mm">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_267952481" STARTINCLINATION="136;0;" ENDINCLINATION="147;0;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<node TEXT="linked map and markdown" ID="ID_814928117" CREATED="1583490353109" MODIFIED="1583490375266" LINK="/home/romilly/git/active/linked-markdown-and-mindmap/plan/linked-markdown-and-mindmap-plan.mm"/>
+<node TEXT="pip-marketing" ID="ID_435386860" CREATED="1555261658734" MODIFIED="1641835792199" LINK="../../../../git/active/pip-marketing/plan/pip-marketing-plan.mm"/>
+<node TEXT="zero-web" ID="ID_623098998" CREATED="1548053674198" MODIFIED="1548053713847" LINK="../../../../git/active/zero-web/plan/zero-web-plan.mm"/>
+<node TEXT="fp2md" ID="ID_1319035899" CREATED="1584519092422" MODIFIED="1584519150643" LINK="../../../../git/active/fp2md/plan/fp2md-plan.mm">
+<node TEXT="fp2md-private" ID="ID_1714670748" CREATED="1584627117422" MODIFIED="1584627142503" LINK="../../../../git/active/fp2md-private/plan/fp2md-private-plan.mm"/>
+</node>
+<node TEXT="freemap" ID="ID_889165434" CREATED="1641033840140" MODIFIED="1641033956096"/>
+</node>
+<node TEXT="concerns" POSITION="right" ID="ID_1551496231" CREATED="1483615883909" MODIFIED="1483615935228" LINK="Concerns.mm"/>
+<node TEXT="sknoman" POSITION="left" ID="ID_1662002299" CREATED="1510597895480" MODIFIED="1510597899730">
+<node TEXT="mind mapping" ID="ID_267952481" CREATED="1541333262648" MODIFIED="1541338960133"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Improve ease of discovery + content value
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      Using categories and contexts: not (just) hierarchical, maybe using distance metric of some sort.
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      Look at sell orderings of features/subsets?
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="snow-sna" ID="ID_485328873" CREATED="1510597900768" MODIFIED="1510597929034" LINK="../../../../git/active/skno-sna/plan/skno-sna-plan.mm"/>
+<node TEXT="infrastructure" ID="ID_342916139" CREATED="1511197241251" MODIFIED="1541333222578" LINK="../../../../git/active/inftastructure/plan/inftastructure-plan.mm"/>
+<node TEXT="paperchase-skno" ID="ID_815492122" CREATED="1547573546886" MODIFIED="1547573596266" LINK="../../../../git/active/paperchase-skno/plan/paperchase-skno-plan.mm"/>
+<node TEXT="feedmail" ID="ID_313421618" CREATED="1548052754197" MODIFIED="1548052800208" LINK="../../../../git/active/feedmail/plan/feedmail-plan.mm"/>
+<node TEXT="gmail-skno" ID="ID_1639630513" CREATED="1549807799366" MODIFIED="1549807831385" LINK="../../../../git/active/gmail-skno/plan/gmail-skno-plan.mm"/>
+<node TEXT="wikipedia-skno" ID="ID_822173163" CREATED="1556008708756" MODIFIED="1556008767159" LINK="../../../../git/active/wikipedia-skno/plan/wikipedia-skno-plan.mm"/>
+<node TEXT="paperchase" ID="ID_531854740" CREATED="1560253791452" MODIFIED="1560253832483" LINK="../../../../git/active/paperchase/plan/paperchase-plan.mm"/>
+<node TEXT="simple scholar" ID="ID_1884913600" CREATED="1596288396743" MODIFIED="1641898921977" LINK="/home/romilly/git/active/simple-scholar/plan/simple-scholar-plan.mm"/>
+</node>
+<node TEXT="personal" POSITION="left" ID="ID_977388348" CREATED="1505045161542" MODIFIED="1505045168721">
+<node TEXT="study tidy" ID="ID_1448718191" CREATED="1505400034835" MODIFIED="1641898995084" LINK="/home/romilly/git/active/study-tidy/plan/study-tidy-plan.mm"/>
+<node TEXT="Kent move" ID="ID_441211348" CREATED="1520778063117" MODIFIED="1641899039743" LINK="../../../../git/active/Kent-move/plan/Kent-move-plan.mm"/>
+<node TEXT="flat contents insurance" ID="ID_663975360" CREATED="1527066854925" MODIFIED="1527066901352" LINK="../../../../git/active/flat-content-insurance/plan/flat-content-insurance-plan.mm"/>
+<node TEXT="HMS Amethyst" ID="ID_309657794" CREATED="1542636657152" MODIFIED="1542636721653" LINK="../../../../git/active/Amethyst/plan/Amethyst.mm"/>
+<node TEXT="alex-pi" ID="ID_629449638" CREATED="1562230785002" MODIFIED="1562230809437" LINK="../../../../git/active/alex-pi/plan/alex-pi-plan.mm"/>
+<node TEXT="energenie" ID="ID_1539980010" CREATED="1579348375643" MODIFIED="1579348401983" LINK="../../../../git/active/energenie/plan/energenie-plan.mm"/>
+<node TEXT="devx60 - 60 years a developer" ID="ID_1767515693" CREATED="1581237946158" MODIFIED="1581237995929" LINK="../../../../git/active/devx60/plan/devx60-plan.mm"/>
+<node TEXT="mygame" ID="ID_1836454434" CREATED="1593324766653" MODIFIED="1593324810049" LINK="../../../../git/active/mygame/plan/mygame-plan.mm"/>
+<node TEXT="retirement move" ID="ID_1380585287" CREATED="1594377997096" MODIFIED="1594378043091" LINK="../../../../git/active/retirement-move/plan/retirement-move-plan.mm">
+<node TEXT="planning objection" ID="ID_517787183" CREATED="1599489194067" MODIFIED="1641899982648" LINK="/home/romilly/git/active/planning-objection/plan/planning-objection-plan.mm"/>
+</node>
+<node TEXT="wesnoth editor" ID="ID_1230791651" CREATED="1605700296455" MODIFIED="1605700338776" LINK="../../../../git/active/wesnoth-editor/plan/wesnoth-editor-plan.mm"/>
+<node TEXT="tidying up" ID="ID_1199767312" CREATED="1607075026919" MODIFIED="1607075067088" LINK="../../../../git/active/tidy-up/plan/tidy-up-plan.mm"/>
+<node TEXT="Ghana Schools" ID="ID_933897435" CREATED="1609600850800" MODIFIED="1609600875244" LINK="../../../../git/active/ghana-schools/plan/ghana-schools-plan.mm"/>
+<node TEXT="level up" ID="ID_442020486" CREATED="1611830861476" MODIFIED="1611830888260" LINK="../../../../git/active/level-up/plan/level-up-plan.mm"/>
+<node TEXT="blog import" ID="ID_1444082907" CREATED="1611831022474" MODIFIED="1611831116169" LINK="../../../../git/active/website-blog-import/blog-import-plan.mm"/>
+</node>
+<node TEXT="robot" POSITION="left" ID="ID_1713185638" CREATED="1613121267771" MODIFIED="1613121275721">
+<node TEXT="https://www.ghostrobotics.io/" ID="ID_1524475324" CREATED="1613121277542" MODIFIED="1613121277542" LINK="https://www.ghostrobotics.io/"/>
+</node>
+<node TEXT="resources" POSITION="right" ID="ID_1732049432" CREATED="1556971939435" MODIFIED="1556971972945" LINK="../../../resources/Resources.mm"/>
+<node TEXT="learning" POSITION="left" ID="ID_485318609" CREATED="1574668530098" MODIFIED="1574668544022">
+<node TEXT="Languages" ID="ID_502098529" CREATED="1517129603017" MODIFIED="1517129607999">
+<node TEXT="mandarin" ID="ID_758125472" CREATED="1573389782291" MODIFIED="1593084398658" LINK="../../../../git/active/languages/plan/mandarin-plan.mm"/>
+<node TEXT="french" ID="ID_241876142" CREATED="1593084228955" MODIFIED="1593084268342" LINK="../../../../git/active/languages/plan/accentuate-plan.mm"/>
+</node>
+<node TEXT="maths" ID="ID_1170454671" CREATED="1574668026289" MODIFIED="1574668040301" LINK="../../../../git/active/maths/plan/MathStudy.mm">
+<node TEXT="probability" ID="ID_1307503076" CREATED="1610889531819" MODIFIED="1610889543980">
+<node TEXT="jaynes" ID="ID_1804533555" CREATED="1610889552599" MODIFIED="1610889584084" LINK="../../../../git/active/jaynes-probability/plan/jaynes-probability-plan.mm"/>
+</node>
+<node TEXT="math for ann" ID="ID_328161198" CREATED="1610960364533" MODIFIED="1610960408629" LINK="../../../../git/active/math-for-ann/plan/math-for-ann-plan.mm"/>
+</node>
+<node TEXT="crepe audio" ID="ID_1070201183" CREATED="1574668555801" MODIFIED="1574668596832" LINK="../../../../git/active/crepe-audio/plan/crepe-audio-plan.mm"/>
+<node TEXT="book-notes" ID="ID_898925319" CREATED="1577729622548" MODIFIED="1577729639595" LINK="../../../../git/active/book-notes/plan/book-notes-plan.mm"/>
+<node TEXT="LPC1114" ID="ID_558670458" CREATED="1582984322573" MODIFIED="1641900648959" LINK="../../../../git/active/lpc1114/lpc1114-plan/plan/lpc1114-plan.mm"/>
+<node TEXT="APL remastered" ID="ID_1171000018" CREATED="1610205643642" MODIFIED="1610205658396" LINK="../../../../git/active/apl-remastered/plan/apl-remastered-plan.mm"/>
+<node TEXT="Python Data Science Courses" ID="ID_375282857" CREATED="1639750030784" MODIFIED="1639750030784" LINK="../../../../git/active/data-science-python/plan/PythonDataScienceCourses.mm">
+<edge STYLE="bezier" WIDTH="thin"/>
+</node>
+</node>
+</node>
+</map>
